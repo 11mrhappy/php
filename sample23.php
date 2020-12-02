59b089a69a8c0592db30cc1295f07f883a23dc00
11mrhappy <nekomamiremy@gmail.com>
2020-12-02 23:51:41 +0900

    余剰演算 week

---
+++ sample23.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+  <?php
+  $week = ['金','土','日','月','火','水','木'];
+
+  for($i=0; $i<7; $i++){
+    print($week[$i] . "\n");
+  }
+  
+  for($i=1; $i<31; $i++){
+    print(" " . $week[$i%7] . "\n");
+  }
+  ?>
+</pre>
+</main>
+</body>    
+</html>
