bc4581521e5658a6db205ce27022118c035025a6
11mrhappy <nekomamiremy@gmail.com>
2020-12-02 23:35:11 +0900

    全角=>半角

--- sample20.php
+++ sample20.php
@@ -19,16 +19,22 @@
 <h2>Practice</h2>
 <pre>
   <?php
-  $age = 20;
-
+  $age = '２０';
+  
+  $age = mb_convert_kana($age, 'n', 'UTF-8')
   if(is_numeric($age)){
     print($age . '歳');
   } else{
     print('年齢が数字ではありません');
   }
   ?>
+
   <br>
-  is_numericは数値かどうかを判断する、数値ならtrue
+  is_numericは数値(半角)かどうかを判断する、数値ならtrue
+  <br>
+  全角数字を半角数字に直すにはmb_convert_kanaを使い、変数に変更後の値を再代入する
+  <br>
+  nで半角数字に変更できる、UTF-8は使ってる文字コード
 </pre>
 </main>
 </body>    
